b30122e44c82c094151c1a4e558b6e99b66e9e93
Adam Peplinski <adam@mech.kth.se>
2017-10-23 12:00:20 +0200

    Upgraded noise box module

--- utility/forcing/noise_box/noisebx.f
+++ utility/forcing/noise_box/noisebx.f
@@ -4,131 +4,176 @@
 !! @author Adam Peplinski
 !! @date Feb 2, 2017
 !=======================================================================
-!> @brief Set noise_box parameters
+!> @brief Register noise_box module
 !! @ingroup noise_box
-      subroutine nse_box_param_get
+!! @note This routine should be called in userchk during first step
+!!  between calls to frame_start and frame_rparam
+      subroutine nseb_register()
       implicit none
 
       include 'SIZE'
       include 'INPUT'
-      include 'PARALLEL'        ! ISIZE, WDSIZE, LSIZE,CSIZE
+      include 'RPRMD'
+      include 'FRAMELP'
       include 'NOISEBXD'
 
 !     local variables
-      integer il, ip  ! loop index
-!     dictionary operations
-      integer ifnd, i_out
-      real d_out
-      character*132 lkey
-      logical ifsec
-!     for broadcasting
-      integer llen, lsent
-      parameter (lsent = (2+2*LDIM))
-      real rtmp(lsent)
+      integer lpmid
+      real ltim
+
+!     functions
+      real dnekclock
 !-----------------------------------------------------------------------
-!     default values
-      NSEB_TIM = 0.0
-      NSEB_AMP = 0.0
-      do il=1,NDIM
-         NSEB_BMIN(il) = 0.0
-         NSEB_BMAX(il) = 0.0
-      enddo
+!     timing
+      ltim = dnekclock()
 
-!     dictionary
-      if (NID.eq.0) then
-!     check consistency
-         call rprm_check(nseb_nkeys, nseb_dictkey, nseb_n3dkeys,
-     $           nseb_l3dkey, ifsec)
-
-!     if section present read parameters
-         if (ifsec) then
-!     time
-            lkey = trim(adjustl(nseb_dictkey(1)))//':'//
-     $             trim(adjustl(nseb_dictkey(2)))
-            call finiparser_getDbl(d_out,trim(lkey),ifnd)
-            if (ifnd.eq.1) then
-               nseb_tim = abs(d_out)
-            endif
-
-!     amplitude
-            lkey = trim(adjustl(nseb_dictkey(1)))//':'//
-     $             trim(adjustl(nseb_dictkey(3)))
-            call finiparser_getDbl(d_out,trim(lkey),ifnd)
-            if (ifnd.eq.1) then
-               nseb_amp = abs(d_out)
-            endif
-
-!     box corners
-            do il=1,NDIM
-               lkey = trim(adjustl(nseb_dictkey(1)))//':'//
-     $                trim(adjustl(nseb_dictkey(3+il)))
-               call finiparser_getDbl(d_out,trim(lkey),ifnd)
-               if (ifnd.eq.1) then
-                  nseb_bmin(il) = d_out
-               endif
-            enddo
-
-!     box corners
-            do il=1,NDIM
-               lkey = trim(adjustl(nseb_dictkey(1)))//':'//
-     $                trim(adjustl(nseb_dictkey(6+il)))
-               call finiparser_getDbl(d_out,trim(lkey),ifnd)
-               if (ifnd.eq.1) then
-                  nseb_bmax(il) = d_out
-               endif
-            enddo
-         endif
-
-!     print prarameters values
-         write(*,*) '[',trim(nseb_dictkey(1)),']'
-         write(*,*) trim(nseb_dictkey(2)),' = ',nseb_tim
-         write(*,*) trim(nseb_dictkey(3)),' = ',nseb_amp
-         do il=1,NDIM
-            write(*,*) trim(nseb_dictkey(3+il)),' = ',nseb_bmin(il)
-         enddo
-         do il=1,NDIM
-            write(*,*) trim(nseb_dictkey(6+il)),' = ',nseb_bmax(il)
-         enddo
-      endif ! NID
-
-!     broadcast data
-      if (NID.eq.0) then
-         ip = 1
-         rtmp(ip) = NSEB_TIM
-         ip = ip +1
-         rtmp(ip) = NSEB_AMP
-         do il=1,NDIM
-            ip = ip +1
-            rtmp(ip) = NSEB_BMIN(il)
-         enddo
-         do il=1,NDIM
-            ip = ip +1
-            rtmp(ip) = NSEB_BMAX(il)
-         enddo
+!     find parent module
+      call mntr_mod_is_name_reg(lpmid,'FRAME')
+!     register module
+      call mntr_mod_reg(nseb_id,lpmid,nseb_name,
+     $      'Adding noise in rectangular domain')
+      if (lpmid.le.0) then
+         call mntr_log(nseb_id,lp_vrb,
+     $        'ERROR: parent module ['//'FRAME'//'] not registered')
       endif
-      llen = lsent*WDSIZE
-      call bcast(rtmp,llen)
-      if (NID.ne.0) then
-         ip = 1
-         NSEB_TIM = rtmp(ip)
-         ip = ip +1
-         NSEB_AMP = rtmp(ip)
-         do il=1,NDIM
-            ip = ip +1
-            NSEB_BMIN(il) = rtmp(ip)
-         enddo
-         do il=1,NDIM
-            ip = ip +1
-            NSEB_BMAX(il) = rtmp(ip)
-         enddo
-      endif
+
+!     register timer
+      call mntr_tmr_is_name_reg(lpmid,'FRM_TOT')
+      call mntr_tmr_reg(nseb_tmr_id,lpmid,nseb_id,
+     $      'NSEB_TOT','Noise box total time')
+
+!     register and set active section
+      call rprm_sec_reg(nseb_sec_id,nseb_id,'_'//adjustl(nseb_name),
+     $     'Runtime paramere section for noise_box module')
+      call rprm_sec_set_act(.true.,nseb_sec_id)
+
+!     register parameters
+      call rprm_rp_reg(nseb_tim_id,nseb_sec_id,'TIME',
+     $     'Time to add noise',rprm_par_real,0,0.0,.false.,' ')
+
+      call rprm_rp_reg(nseb_amp_id,nseb_sec_id,'AMPLITUDE',
+     $     'Noise amplitude',rprm_par_real,0,0.0,.false.,' ')
+
+      call rprm_rp_reg(nseb_bmin_id(1),nseb_sec_id,'BOXMINX',
+     $     'Position of lower left box corner; dimension X ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+      call rprm_rp_reg(nseb_bmin_id(2),nseb_sec_id,'BOXMINY',
+     $     'Position of lower left box corner; dimension Y ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+      if (IF3D) call rprm_rp_reg(nseb_bmin_id(ndim),nseb_sec_id,
+     $     'BOXMINZ','Position of lower left box corner; dimension Z ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+      call rprm_rp_reg(nseb_bmax_id(1),nseb_sec_id,'BOXMAXX',
+     $     'Position of upper right box corner; dimension X ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+      call rprm_rp_reg(nseb_bmax_id(2),nseb_sec_id,'BOXMAXY',
+     $     'Position of upper right box corner; dimension Y ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+      if (IF3D) call rprm_rp_reg(nseb_bmax_id(ndim),nseb_sec_id,
+     $     'BOXMAXZ','Position of upper right box corner; dimension Z ',
+     $     rprm_par_real,0,0.0,.false.,' ')
+
+!     timing
+      ltim = dnekclock() - ltim
+      call mntr_tmr_add(nseb_tmr_id,1,ltim)
 
       return
       end
 !=======================================================================
+!> @brief Initilise noise_box module
+!! @ingroup noise_box
+!! @note This routine should be called in userchk during first step
+!!    after call to frame_rparam
+!! @remark This routine uses global scratch space \a SCRUZ
+      subroutine nseb_init()
+      implicit none
+
+      include 'SIZE'
+      include 'INPUT'
+      include 'RPRMD'
+      include 'FRAMELP'
+      include 'NOISEBXD'
+
+!     local variables
+      integer ierr, nhour, nmin
+      integer itmp
+      real rtmp, ltim
+      logical ltmp
+      character*20 ctmp
+
+!     functions
+      real dnekclock
+!-----------------------------------------------------------------------
+!     timing
+      ltim = dnekclock()
+
+!     get runtime parameters
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_tim_id,rprm_par_real)
+      nseb_tim = rtmp
+
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_amp_id,rprm_par_real)
+      nseb_amp = rtmp
+
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmin_id(1),
+     $     rprm_par_real)
+      nseb_bmin(1) = rtmp
+
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmin_id(2),
+     $     rprm_par_real)
+      nseb_bmin(2) = rtmp
+
+      if (IF3D) then
+         call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmin_id(ndim),
+     $     rprm_par_real)
+         nseb_bmin(ndim) = rtmp
+      endif
+
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmax_id(1),
+     $     rprm_par_real)
+      nseb_bmax(1) = rtmp
+
+      call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmax_id(2),
+     $     rprm_par_real)
+      nseb_bmax(2) = rtmp
+
+      if (IF3D) then
+         call rprm_rp_get(itmp,rtmp,ltmp,ctmp,nseb_bmax_id(ndim),
+     $     rprm_par_real)
+         nseb_bmax(ndim) = rtmp
+      endif
+
+!     is everything initialised
+      nseb_ifinit=.true.
+
+!     timing
+      ltim = dnekclock() - ltim
+      call mntr_tmr_add(nseb_tmr_id,1,ltim)
+
+      return
+      end
+!=======================================================================
+!> @brief Check if module was initialised
+!! @ingroup noise_box
+!! @return nseb_is_initialised
+      logical function nseb_is_initialised()
+      implicit none
+
+      include 'SIZE'
+      include 'NOISEBXD'
+!-----------------------------------------------------------------------
+      nseb_is_initialised = nseb_ifinit
+
+      return
+      end function
+!=======================================================================
 !> @brief Add noise to velocity field in a box
 !! @ingroup noise_box
-      subroutine nse_box_add()
+      subroutine nseb_noise_add()
       implicit none
 
       include 'SIZE'            ! NX1, NY1, NZ1, NELV, NID
@@ -137,6 +182,7 @@
       include 'INPUT'           ! IF3D
       include 'SOLN'            ! VX, VY, VZ, VMULT
       include 'GEOM'            ! XM1, YM1, ZM1
+      include 'FRAMELP'
       include 'NOISEBXD'
 
 !     local variables
@@ -144,15 +190,19 @@
       real xl(LDIM)
       logical ifadd
       real fcoeff(3)            !< coefficients for random distribution
+      real ltim
 
 !     functions
-      real mth_rand
+      real dnekclock, mth_rand
 !-----------------------------------------------------------------------
 !     add noise
-      if (NSEB_AMP.gt.0.0) then
-         if (NSEB_TIM.ge.TIME.and.NSEB_TIM.le.(TIME+DT)) then
+      if (nseb_amp.gt.0.0) then
+         if (nseb_tim.ge.TIME.and.nseb_tim.le.(TIME+DT)) then
 
-            if (NIO.eq.0) write(6,*) 'Adding noise; eps= ', NSEB_AMP
+!     timing
+            ltim = dnekclock()
+            call mntr_log(nseb_id,lp_inf,
+     $          "Adding noise to velocity field")
 
             do iel=1,NELV
                do kl=1,NZ1
@@ -164,8 +214,8 @@
                         if (IF3D) xl(NDIM) = ZM1(il,jl,kl,iel)
                         ifadd = .TRUE.
                         do nl=1,NDIM
-                           if (xl(nl).lt.NSEB_BMIN(nl).or.
-     $                          xl(nl).gt.NSEB_BMAX(nl)) then
+                           if (xl(nl).lt.nseb_bmin(nl).or.
+     $                          xl(nl).gt.nseb_bmax(nl)) then
                               ifadd = .FALSE.
 !                              exit
                               goto 20
@@ -177,19 +227,19 @@
                            fcoeff(1)=  3.0e4
                            fcoeff(2)= -1.5e3
                            fcoeff(3)=  0.5e5
-                           VX(il,jl,kl,iel)=VX(il,jl,kl,iel)+NSEB_AMP*
+                           VX(il,jl,kl,iel)=VX(il,jl,kl,iel)+nseb_amp*
      $                          mth_rand(il,jl,kl,ieg,xl,fcoeff)
                            fcoeff(1)=  2.3e4
                            fcoeff(2)=  2.3e3
                            fcoeff(3)= -2.0e5
-                           VY(il,jl,kl,iel)=VY(il,jl,kl,iel)+NSEB_AMP*
+                           VY(il,jl,kl,iel)=VY(il,jl,kl,iel)+nseb_amp*
      $                          mth_rand(il,jl,kl,ieg,xl,fcoeff)
                            if (IF3D) then
                               fcoeff(1)= 2.e4
                               fcoeff(2)= 1.e3
                               fcoeff(3)= 1.e5
                               VZ(il,jl,kl,iel)=VZ(il,jl,kl,iel)+
-     $                             NSEB_AMP*
+     $                             nseb_amp*
      $                             mth_rand(il,jl,kl,ieg,xl,fcoeff)
                            endif
                         endif
@@ -203,6 +253,11 @@
 !     face averaging
             call opdssum(VX,VY,VZ)
             call opcolv (VX,VY,VZ,VMULT)
+
+!     timing
+            ltim = dnekclock() - ltim
+            call mntr_tmr_add(nseb_tmr_id,1,ltim)
+
          endif
       endif
 
