ab9b4856a28bcb683929c1e2a7a391c94f548dc5
Adam Peplinski <adam@mech.kth.se>
2019-04-09 09:46:13 +0200

    Unified comments format

--- tools/stat/stat_IO.f
+++ tools/stat/stat_IO.f
@@ -33,54 +33,54 @@
       include 'INPUT'           ! ifpo
       include 'SOLN'            ! pr
       include 'STATD'           ! 
-      
-!     local variables
+
+      ! local variables
       logical ifpo_tmp
 !----------------------------------------------------------------------
       ifpo_tmp = ifpo
       ifpo=.FALSE.
 
-!     Fields to outpost: <u>t, <v>t, <w>t, <p>t
+      ! Fields to outpost: <u>t, <v>t, <w>t, <p>t
       call outpost(stat_ruavg(1,1,1),stat_ruavg(1,1,2),
      $     stat_ruavg(1,1,3),pr,stat_ruavg(1,1,4),'s01')
-      
-!     Fields to outpost: <uu>t, <vv>t, <ww>t, <pp>t
+
+      ! Fields to outpost: <uu>t, <vv>t, <ww>t, <pp>t
       call outpost(stat_ruavg(1,1,5),stat_ruavg(1,1,6),
      $     stat_ruavg(1,1,7),pr,stat_ruavg(1,1,8),'s02')
-      
-!     Fields to outpost: <uv>t, <vw>t,<uw>t, <pu>t
+
+      ! Fields to outpost: <uv>t, <vw>t,<uw>t, <pu>t
       call outpost(stat_ruavg(1,1,9),stat_ruavg(1,1,10),
      $     stat_ruavg(1,1,11),pr,stat_ruavg(1,1,12),'s03')
 
-!     Fields to outpost: <pv>t, <pw>t, <pdudx>t, <pdudy>t
+      ! Fields to outpost: <pv>t, <pw>t, <pdudx>t, <pdudy>t
       call outpost(stat_ruavg(1,1,13),stat_ruavg(1,1,14),
      $     stat_ruavg(1,1,15),pr,stat_ruavg(1,1,16),'s04')
-      
-!     Fields to outpost: <pdudz>t, <pdvdx>t, <pdvdy>t, <pdvdz>t
+
+      ! Fields to outpost: <pdudz>t, <pdvdx>t, <pdvdy>t, <pdvdz>t
       call outpost(stat_ruavg(1,1,17),stat_ruavg(1,1,18),
      $     stat_ruavg(1,1,19),pr,stat_ruavg(1,1,20),'s05')
 
-!     Fields to outpost: <pdwdx>t, <pdwdy>t, <pdwdz>t, <uuu>t
+      !Fields to outpost: <pdwdx>t, <pdwdy>t, <pdwdz>t, <uuu>t
       call outpost(stat_ruavg(1,1,21),stat_ruavg(1,1,22),
      $     stat_ruavg(1,1,23),pr,stat_ruavg(1,1,24),'s06')
 
-!     Fields to outpost:  <vvv>t, <www>t, <uuv>t, <uuw>t
+      ! Fields to outpost:  <vvv>t, <www>t, <uuv>t, <uuw>t
       call outpost(stat_ruavg(1,1,25),stat_ruavg(1,1,26),
      $     stat_ruavg(1,1,27),pr,stat_ruavg(1,1,28),'s07')
-      
-!     Fields to outpost: <vvu>t, <vvw>t,  <wwu>t, <wwv>t
+
+      ! Fields to outpost: <vvu>t, <vvw>t,  <wwu>t, <wwv>t
       call outpost(stat_ruavg(1,1,29),stat_ruavg(1,1,30),
      $     stat_ruavg(1,1,31),pr,stat_ruavg(1,1,32),'s08')
       
-!     Fields to outpost:  <ppp>t, <pppp>t, <uvw>t, <uuuu>t
+      ! Fields to outpost:  <ppp>t, <pppp>t, <uvw>t, <uuuu>t
       call outpost(stat_ruavg(1,1,33),stat_ruavg(1,1,34),
      $     stat_ruavg(1,1,35),pr,stat_ruavg(1,1,36),'s09')
-      
-!     Fields to outpost: <vvvv>t, <wwww>t, <e11>t, <e22>t
+
+      ! Fields to outpost: <vvvv>t, <wwww>t, <e11>t, <e22>t
       call outpost(stat_ruavg(1,1,37),stat_ruavg(1,1,38),
      $     stat_ruavg(1,1,39),pr,stat_ruavg(1,1,40),'s10')
       
-!     Fields to outpost: <e33>t, <e12>t, <e13>t, <e23>t
+      ! Fields to outpost: <e33>t, <e12>t, <e13>t, <e23>t
       call outpost(stat_ruavg(1,1,41),stat_ruavg(1,1,42),
      $     stat_ruavg(1,1,43),pr,stat_ruavg(1,1,44),'s11')
 
@@ -92,6 +92,7 @@
 !> @brief Statistics muti-file output of 2D data
 !! @ingroup stat
 !! @details This routine is just modification of mfo_outfld
+!! @remark This routine uses global scratch space \a SCRUZ
       subroutine stat_mfo_outfld2D()
       implicit none
 
@@ -103,8 +104,13 @@
       include 'MAP2D'
       include 'STATD'
 
-!     local variables
-!     temporary variables to overwrite global values
+      ! global variablse
+      ! dummy arrays
+      real ur1(lx1,lz1,2*LELT)
+      common /SCRUZ/  ur1
+
+      ! local variables
+      ! temporary variables to overwrite global values
       logical ifreguol          ! uniform mesh
       logical ifxyol            ! write down mesh
       integer wdsizol           ! store global wdsizo
@@ -126,18 +132,14 @@
       real dnbyte               ! byte sum
       real tiostart, tio        ! simple timing
 
-!     dummy arrays
-      real ur1(lx1,lz1,2*LELT)
-      common /SCRUZ/  ur1
-
-!     functions
+      ! functions
       integer igl_running_sum
       real dnekclock_sync, glsum
 !----------------------------------------------------------------------
-!     simple timing
+      ! simple timing
       tiostart=dnekclock_sync()
 
-!     intialise I/O
+      ! intialise I/O
       ifdiro = .false.
 
       ifmpiio = .false.
@@ -163,40 +165,40 @@
         pid1    = min(np-1,pid0+nproc_o-1) !  range of sending procs
       endif
 
-!     save and set global IO variables
-!     no uniform mesh
+      ! save and set global IO variables
+      ! no uniform mesh
       ifreguol = IFREGUO
       IFREGUO = .FALSE.
 
-!     save mesh
+      ! save mesh
       ifxyol = IFXYO
       IFXYO = .TRUE.
 
-!     force double precission
+      ! force double precission
       wdsizol = WDSIZO
-!     for testing
+      ! for testing
       WDSIZO = WDSIZE
 
       nrg = lxo
-      
-!     get number of 2D elements owned by proceesor with smaller nid
+
+      ! get number of 2D elements owned by proceesor with smaller nid
       itmp = map2d_lown
       nel2DB = igl_running_sum(itmp)
       nel2DB = nel2DB - map2d_lown
-!     replace value
+      ! replace value
       nelBl = NELB
       NELB = nel2DB
 
-!     set element size
+      ! set element size
       NXO   = stat_nm2
       NYO   = stat_nm3
       NZO   = 1
       nxyzo = NXO*NYO*NZO
 
-!     if this is AMR run, one has to cast 3D mesh to 2D nonconforming counterpart
-      call stat_mfo_AMR2D
+      ! if this is AMR run, one has to cast 3D mesh to 2D nonconforming counterpart
+      call stat_mfo_crd2D
 
-!     open files on i/o nodes
+      ! open files on i/o nodes
       prefix='sts'
       ierr=0
       if (nid.eq.pid0) call mfo_open_files(prefix,ierr)
@@ -204,23 +206,23 @@
       call mntr_check_abort(stat_id,ierr,
      $     'Error opening file in stat_mfo_outfld2D.')
 
-!     write header, byte key, global ordering
+      ! write header, byte key, global ordering
       call stat_mfo_write_hdr2D
 
-!     initial offset: header, test pattern, global ordering
+      ! initial offset: header, test pattern, global ordering
       offs0 = iHeaderSize + 4 + isize*int(map2d_gnum,8)
       offs = offs0
 
-!     stride
+      ! stride
       strideB = int(nelb,8)*nxyzo*wdsizo
       stride  = int(map2d_gnum,8)*nxyzo*wdsizo
 
-!     count fields
+      ! count fields
       ioflds = 0
 
-!     write coordinates
+      ! write coordinates
       kl = 0
-!     copy vector
+      ! copy vector
       do il=1,map2d_lnum
          if(map2d_own(il).eq.nid) then
             call copy(ur1(1,1,2*kl+1),map2d_xm1(1,1,il),nxyzo)
@@ -229,22 +231,22 @@
          endif
       enddo
 
-!     check consistency
+      ! check consistency
       ierr = 0
       if (kl.ne.map2d_lown) ierr=1
       call mntr_check_abort(stat_id,ierr,'inconsistent map2d_lown 1')
       
-!     offset
+      ! offset
       kl = 2*kl
       offs = offs0 + stride*ioflds + 2*strideB
       call byte_set_view(offs,ifh_mbyte)
       call mfo_outs(ur1,kl,nxo,nyo,nzo)
       ioflds = ioflds + 2
 
-!     write fields
+      ! write fields
       do jl=1,stat_nvar
          kl = 0
-!     copy vector
+         ! copy vector
          do il=1,map2d_lnum
             if(map2d_own(il).eq.nid) then
                kl = kl +1
@@ -252,22 +254,23 @@
             endif
          enddo
 
-!     check consistency
+!
+         ! check consistency
          ierr = 0
          if (kl.ne.map2d_lown) ierr=1
          call mntr_check_abort(stat_id,ierr,'inconsistent map2d_lown 2')
 
-!     offset
+         ! offset
          offs = offs0 + stride*ioflds + strideB
          call byte_set_view(offs,ifh_mbyte)
          call mfo_outs(ur1,kl,nxo,nyo,nzo)
          ioflds = ioflds + 1
       enddo
 
-!     write averaging data
+      ! write averaging data
       call stat_mfo_write_stat2d
 
-!     count bytes
+      ! count bytes
       dnbyte = 1.*ioflds*map2d_lown*wdsizo*nxyzo
 
       ierr = 0
@@ -294,7 +297,7 @@
      &     30X,'avg data-throughput = ',0pf7.1,'MB/s',/,
      &     30X,'io-nodes = ',i5,/)
 
-!     set global IO variables back
+      ! set global IO variables back
       IFREGUO = ifreguol
       IFXYO = ifxyol
       WDSIZO = wdsizol
@@ -306,6 +309,7 @@
 !> @brief Write hdr, byte key, global ordering of 2D data
 !! @ingroup stat
 !! @details This routine is just modification of mfo_write_hdr
+!! @remark This routine uses global scratch space \a CTMP0
       subroutine stat_mfo_write_hdr2D
       implicit none
 
@@ -318,11 +322,12 @@
       include 'MAP2D'
       include 'STATD'
 
-
-!     local variables
-      real*4 test_pattern       ! byte key
+      ! global variable
       integer lglist(0:LELT)    ! dummy array
       common /ctmp0/ lglist
+
+      ! local variables
+      real*4 test_pattern       ! byte key
       integer idum, inelp
       integer nelo              ! number of elements to write
       integer nfileoo           ! number of files to create
@@ -357,34 +362,34 @@
          endif 
       endif
 
-!     write header
+      ! write header
       ierr = 0
       if(nid.eq.pid0) then
          call blank(hdr,132)
 
-!     varialbe set
+         ! varialbe set
          call blank(rdcode1,10)
 
-!     we save coordinates
+         ! we save coordinates
          rdcode1(1)='X'
-!     and set of fields marked as passive scalars
+         ! and set of fields marked as passive scalars
          rdcode1(2) = 'S'
          write(rdcode1(3),'(i1)') stat_nvar/10
          write(rdcode1(4),'(i1)') stat_nvar-(stat_nvar/10)*10
 
-!     no pressure written so pressure format set to false
-      if_press_mesh = .false.
+         ! no pressure written so pressure format set to false
+         if_press_mesh = .false.
          
          write(hdr,1) wdsizo,nxo,nyo,nzo,nelo,map2d_gnum,time,istep,
      $        fid0, nfileoo, (rdcode1(il),il=1,10),p0th,if_press_mesh
  1       format('#std',1x,i1,1x,i2,1x,i2,1x,i2,1x,i10,1x,i10,1x,
      $        e20.13,1x,i9,1x,i6,1x,i6,1x,10a,1pe15.7,1x,l1) 
 
-!     write test pattern for byte swap
+         ! write test pattern for byte swap
          test_pattern = 6.54321 
 
          if(ifmpiio) then
-!     only rank0 (pid00) will write hdr + test_pattern + time list
+            ! only rank0 (pid00) will write hdr + test_pattern + time list
             call byte_write_mpi(hdr,iHeaderSize/4,pid00,ifh_mbyte,ierr)
             call byte_write_mpi(test_pattern,1,pid00,ifh_mbyte,ierr)
          else
@@ -397,8 +402,8 @@
       call mntr_check_abort(stat_id,ierr,
      $     'Error writing header in stat_mfo_write_hdr2D.')
 
-!     write global 2D elements numbering for this group
-!     copy data
+      ! write global 2D elements numbering for this group
+      ! copy data
       lglist(0) = map2d_lown
       kl = 0
       do il=1,map2d_lnum
@@ -407,7 +412,7 @@
             lglist(kl) = map2d_gmap(il)
          endif
       enddo
-!     check consistency
+      ! check consistency
       ierr = 0
       if (kl.ne.map2d_lown) ierr=1
 
@@ -460,7 +465,7 @@
       include 'MAP2D'
       include 'STATD'
 !-----------------------------------------------------------------------
-!     to be added
+      ! to be added
       if(NID.eq.PID0) then
 
 
@@ -469,11 +474,11 @@
       return
       end subroutine
 !=======================================================================
-!> @brief Write nonconforming data to the file
+!> @brief Write element centres to the file (in case of AMR level as well)
 !! @ingroup stat
-      subroutine stat_mfo_AMR2D
+      subroutine stat_mfo_crd2D
       implicit none
-#ifdef NEKP4EST
+
       include 'SIZE'
       include 'INPUT'
       include 'RESTART'
@@ -482,7 +487,9 @@
       include 'WZ'
       include 'MAP2D'
       include 'STATD'
+#ifdef NEKP4EST
       include 'NEKP4EST'
+#endif
 
       ! local variables
       character*3 prefix        ! file prefix
@@ -506,12 +513,11 @@
       integer ierr              ! error mark
       integer ibsw_out, len
       integer*8 ioff            ! offset
-      logical if_press_mesh     ! pessure mesh mark
 
       character*132 hdr         ! header
 !-----------------------------------------------------------------------
       ! open files on i/o nodes
-      prefix='A2D'
+      prefix='c2D'
       ierr=0
       if (nid.eq.pid0) call mfo_open_files(prefix,ierr)
 
@@ -565,7 +571,7 @@
       endif
 
       call mntr_check_abort(stat_id,ierr,
-     $     'Error writing header in stat_mfo_AMR2D.')
+     $     'Error writing header in stat_mfo_crd2D.')
 
       ! write global 2D elements numbering for this group
       ! copy data
@@ -616,12 +622,18 @@
       endif
 
       call mntr_check_abort(stat_id,ierr,
-     $     'Error writing global nums in stat_mfo_AMR2D')
+     $     'Error writing global nums in stat_mfo_crd2D')
 
+#ifdef NEKP4EST
       ! write refinement level; this code is not optimal, but I don't do it often
       do il = 1,NELV
          itmp(map2d_lmap(il)) = NP4_LEVEL(il)
       enddo
+#else
+      do il = 1,NELT
+         itmp(il) = 0
+      enddo
+#endif
       ! copy data
       lglist(0) = map2d_lown
       kl = 0
@@ -666,7 +678,7 @@
       endif
 
       call mntr_check_abort(stat_id,ierr,
-     $     'Error writing refinement level in stat_mfo_AMR2D')
+     $     'Error writing refinement level in stat_mfo_crd2D')
 
       ! get 2D elements cell centres
       ! get interpolation operators
@@ -724,7 +736,7 @@
       endif
 
       call mntr_check_abort(stat_id,ierr,
-     $     'Error writing element centres in stat_mfo_AMR2D')
+     $     'Error writing element centres in stat_mfo_crd2D')
 
       ierr = 0
       if (nid.eq.pid0) then
@@ -735,9 +747,8 @@
          endif
       endif
       call mntr_check_abort(stat_id,ierr,
-     $     'Error closing file in stat_mfo_AMR2D.')
+     $     'Error closing file in stat_mfo_crd2D.')
 
-#endif
       return
       end subroutine
 !=======================================================================
